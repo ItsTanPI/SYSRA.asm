7ae41a03ac2d9c4225ecee8edede581c900501f6
Shri Hari <shriharishanmugam@gmail.com>
2025-03-08 16:16:34 +0530

    PAGE

--- Build.bat
+++
@@ -1,6 +0,0 @@
-masm Game.asm;
-link Game.obj
-nasm Keyboard.asm -o Keyboard.obj -f obj
-tcc -c -IC:\TURBOC3\INCLUDE -LC:\TURBOC3\LIB SYSRA.c
-tlink SYSRA.obj Game.obj keyboard.obj, SYSRA.exe, Game.map, C:\TURBOC3\LIB\C0S.OBJ C:\TURBOC3\LIB\CS.LIB
-SYSRA.exe
